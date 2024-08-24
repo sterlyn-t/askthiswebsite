cac1dc1e70c3e04c9efda8f42ffa200241ca070b
Sterlyn <sterlyntang31@gmail.com>
2024-08-24 18:03:13 -0400

    Chore: Clean-up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,8 +53,8 @@ export default function Home() {
         <div className="max-w-5xl items-center text-2xl gap-8 flex flex-col">
           <div>
             <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center relative z-20 py-2 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
-              Use AI to chat with any webpage today. Paste any URL to <br />
-              <Cover>start chatting</Cover>
+              Use AI to chat about any webpage today. Paste any URL to <br />
+              <Cover>start chatting.</Cover>
             </h1>
           </div>
           <form
